795070b5189fb9612240771ee64104451236e3de
lzw_29107 <2910729822@qq.com>
2025-03-13 15:17:23 +0800

    Test

--- CryptoBinPkg/Driver/Packaging/generate_cryptodriver.py
+++ CryptoBinPkg/Driver/Packaging/generate_cryptodriver.py
@@ -705,12 +705,8 @@ def generate_platform_files(edk2_crypto_ver: str = "1.0"):
         for phase in phases:
             for target in targets:
                 for arch in arches:
-                    if arch in ["ARM","AARCH64"] and phase == "Smm":
-                        continue
-                    if arch in ["ARM","IA32"] and "StandaloneMm" in phase:
-                        continue
-                    if arch == "AARCH64" and phase == "StandaloneMm_MmSupv":
-                        continue
+                    if arch in ["IA32", "ARM", "AARCH64"] and phase in ["Smm", "StandaloneMm_MmSupv"]:
+                continue
                     inf_files.append((flavor, phase, target, arch))
     print(f"Generating {len(inf_files)} inf files")
     # first delete any files that we don't need
@@ -874,9 +870,7 @@ def generate_platform_files(edk2_crypto_ver: str = "1.0"):
         comp_types = get_supported_library_types(phase)
         upper_phase = phase.upper()
         for arch in arches:
-            if arch in ["ARM","AARCH64"] and phase == "Smm":
-                continue
-            if arch in ["ARM","AARCH64","IA32"] and phase == "StandaloneMm_MmSupv":
+            if arch in ["IA32", "ARM", "AARCH64"] and phase in ["Smm", "StandaloneMm_MmSupv"]:
                 continue
             dsc_lines.append(f"!if $({upper_phase}_CRYPTO_ARCH) == {arch}")
             lib_class_str = ", ".join(map(lambda x: ".".join(
